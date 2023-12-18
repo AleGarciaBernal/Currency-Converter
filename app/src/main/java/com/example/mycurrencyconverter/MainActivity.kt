253511762d3083f3a5cd9582402089964ce771ab
aleandy7 <80707995+AleGarciaBernal@users.noreply.github.com>
2023-12-17 23:27:38 -0400

    Currency Converter v1.1.1 API

--- app/src/main/java/com/example/mycurrencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/mycurrencyconverter/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.example.mycurrencyconverter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.mycurrencyconverter.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
+import org.json.JSONException
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 //
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var resultTv: TextView
     private lateinit var solutionTv: TextView
+    private var queue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         resultTv = findViewById(R.id.result_tv)
         solutionTv = findViewById(R.id.solution_tv)
-
         val buttonIds = arrayOf(
             //R.id.button_c,
             R.id.button_open_bracket, R.id.button_close_bracket,
@@ -28,10 +35,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.button_4, R.id.button_5, R.id.button_6, R.id.button_7, R.id.button_8, R.id.button_9,
             R.id.button_ac, R.id.button_dot
         )
-
         buttonIds.forEach { id ->
             findViewById<MaterialButton>(id).setOnClickListener(this)
         }
+        //obtenerDatosVolley()
+        queue = Volley.newRequestQueue(this)
+        obtenerDatosVolley()
+        //mytextviewprueba = findViewById(R.id.solution_tv)
+    }
+    private fun obtenerDatosVolley() {
+        val url = "https://v6.exchangerate-api.com/v6/e4fe3f82f3082b7484c4f5df/latest/BOB"
+        val request = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            { response -> solutionTv!!.text = response.toString() }) {
+            Log.d(
+                "MainActivity",
+                "onErrorResponse: ERROR"
+            )
+        }
+        queue!!.add(request)
     }
 
     override fun onClick(view: View) {
@@ -44,6 +68,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 solutionTv.text = ""
                 resultTv.text = "0"
                 return
+
             }
             "=" -> {
                 solutionTv.text = resultTv.text
